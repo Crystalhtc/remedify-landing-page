dfda93b841a5c8763e62e05d16688ac73d179163
madoka <mnogaki@my.bcit.ca>
2024-11-18 21:00:39 -0800

    adjust the layout of the features section

--- src/app/components/Features/Features.jsx
+++ src/app/components/Features/Features.jsx
@@ -2,42 +2,44 @@ import styles from './Features.module.css';
 
 export default function Features() {
     return(
-        <section className={styles.featuresContainer} id='features'>
+        <section className={styles.outerContainer} id='features'>
             <h1 className={styles.title} >Features</h1>
-            <div className={`${styles.singleFeatureContainer} ${styles.singleFeatureContainerReversed}`}>
-                <div className={styles.img} >
-                    <img src="/med-reminders.svg" alt="Notification screen" />
+            <div className={styles.featuresContainer}>
+                <div className={`${styles.singleFeatureContainer} ${styles.singleFeatureContainerReversed}`}>
+                    <div className={styles.img} >
+                        <img src="/med-reminders.svg" alt="Notification screen" />
+                    </div>
+                    <div className={styles.description}>
+                        <h1>Medication Reminders</h1>
+                        <p>Receive timely, simple reminders designed to fit your routine, helping you stick to your medication schedule effortlessly.</p>
+                    </div>
                 </div>
-                <div className={styles.description}>
-                    <h1>Medication Reminders</h1>
-                    <p>Receive timely, simple reminders designed to fit your routine, helping you stick to your medication schedule effortlessly.</p>
+                <div className={styles.singleFeatureContainer}>
+                    <div className={styles.description}>
+                        <h1>One-Tap Medication Logging</h1>
+                        <p>Log each dose with just one tap. Our intuitive interface ensures that tracking your medications is quick and uncomplicated.</p>
+                    </div>
+                    <div className={styles.img} >
+                        <img src="/med-logging.svg" alt="Loggin med card" />
+                    </div>
                 </div>
-            </div>
-            <div className={styles.singleFeatureContainer}>
-                <div className={styles.description}>
-                    <h1>One-Tap Medication Logging</h1>
-                    <p>Log each dose with just one tap. Our intuitive interface ensures that tracking your medications is quick and uncomplicated.</p>
+                <div className={`${styles.singleFeatureContainer} ${styles.singleFeatureContainerReversed}`}>
+                    <div className={styles.img} >
+                        <img src="/scanning.svg" alt="Med scanning screen" />
+                    </div>
+                    <div className={styles.description}>
+                        <h1>AI-Powered Label Scanning</h1>
+                        <p>Easily scan your medication labels to set up reminders automatically, minimizing the steps you need to remember and simplifying the process.</p>
+                    </div>
                 </div>
-                <div className={styles.img} >
-                    <img src="/med-logging.svg" alt="Loggin med card" />
-                </div>
-            </div>
-            <div className={`${styles.singleFeatureContainer} ${styles.singleFeatureContainerReversed}`}>
-                <div className={styles.img} >
-                    <img src="/scanning.svg" alt="Med scanning screen" />
-                </div>
-                <div className={styles.description}>
-                    <h1>AI-Powered Label Scanning</h1>
-                    <p>Easily scan your medication labels to set up reminders automatically, minimizing the steps you need to remember and simplifying the process.</p>
-                </div>
-            </div>
-            <div className={styles.singleFeatureContainer}>
-                <div className={styles.description}>
-                    <h1>Accessible Medication Library</h1>
-                    <p>Review all your medications in one clear, accessible library. Detailed descriptions and dosage instructions make it easy to understand what you’re taking.</p>
-                </div>
-                <div className={styles.img}>
-                    <img src="/med-library.svg" alt="Med library screen" />
+                <div className={styles.singleFeatureContainer}>
+                    <div className={styles.description}>
+                        <h1>Accessible Medication Library</h1>
+                        <p>Review all your medications in one clear, accessible library. Detailed descriptions and dosage instructions make it easy to understand what you’re taking.</p>
+                    </div>
+                    <div className={styles.img}>
+                        <img src="/med-library.svg" alt="Med library screen" />
+                    </div>
                 </div>
             </div>
         </section>
