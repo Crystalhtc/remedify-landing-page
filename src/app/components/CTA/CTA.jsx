25a1df5279b200343c98b15b0172cbd9c7b2f5be
crystalhtc <crystalhtc01@gmail.com>
2024-11-19 22:58:26 -0800

    update cta graphic file name

--- src/app/components/CTA/CTA.jsx
+++ src/app/components/CTA/CTA.jsx
@@ -11,7 +11,7 @@ export default function CTA() {
             
             <div className={styles.cta} id='cta'>
                 <div className={styles.ctaGraphic}>
-                    <img src='/cta-graphic.svg' alt='CTA graphic' className={styles.graphic} width="420"/>
+                    <img src='/cta-graphic.png' alt='CTA graphic' className={styles.graphic} width="420"/>
                 </div>
                 <div className={styles.ctaText}>
                     <p>Join Us in Making Medication Management Easier and More Accessible</p>
