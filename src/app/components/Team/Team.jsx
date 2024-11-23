9520d85e343956981dc94d1f70b90d4dc845b908
madoka <mnogaki@my.bcit.ca>
2024-11-22 23:39:32 -0800

    update members' photos

--- src/app/components/Team/Team.jsx
+++ src/app/components/Team/Team.jsx
@@ -6,26 +6,26 @@ export default function Team() {
             <h1 className={styles.title}>Meet Remedify Team</h1>
             <div className={styles.membersContainer}>
                 <div className={styles.member}>
-                    <img src='/brian.png' alt='Brian Paragas' />
+                    <img src='/team/brian.png' alt='Brian Paragas' />
                     <h3>Brian Paragas</h3>
                     <p>Project Manager</p>
                     <p>Lead Graphic Designer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/brianparagas/' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
                 </div>
                 <div className={styles.member}>
-                    <img src='/crystal.png' alt='Crystal Cheung' />
+                    <img src='/team/crystal.png' alt='Crystal Cheung' />
                     <h3>Crystal Cheung</h3>
                     <p>Lead UI/UX Designer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/crystal-cheunghtc/' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
                 </div>
                 <div className={styles.member}>
-                    <img src='/nathan.png' alt='Nathan Yee' />
+                    <img src='/team/nathan.png' alt='Nathan Yee' />
                     <h3>Nathan Yee</h3>
                     <p>Lead Developer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/yeenathan/' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
                 </div>
                 <div className={styles.member}>
-                    <img src='/madoka.png' alt='Madoka Nogaki' />
+                    <img src='/team/madoka.png' alt='Madoka Nogaki' />
                     <h3>Madoka Nogaki</h3>
                     <p>UI/UX Designer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/madoka-nogaki/' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
@@ -33,25 +33,25 @@ export default function Team() {
             </div>
             <div className={styles.membersContainer}>
                 <div className={styles.member}>
-                    <img src='/kenny.png' alt='Kenny Harman' />
+                    <img src='/team/kenny.png' alt='Kenny Harman' />
                     <h3>Kenny Harman</h3>
                     <p>Graphic Designer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/kennyharman/' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
                 </div>
                 <div className={styles.member}>
-                    <img src='/deluka.png' alt='Deluka LGH' />
+                    <img src='/team/deluka.png' alt='Deluka LGH' />
                     <h3>Deluka LGH</h3>
                     <p>Developer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/delukalgh/' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
                 </div>
                 <div className={styles.member}>
-                    <img src='/jillian.png' alt='Jillian Gonzales' />
+                    <img src='/team/jillian.png' alt='Jillian Gonzales' />
                     <h3>Jillian Gonzales</h3>
                     <p>UX/UI Designer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/jilliangz' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
                 </div>
                 <div className={styles.member}>
-                    <img src='/yeongtaek.png' alt='Yeongtaek Lee' />
+                    <img src='/team/yeongtaek.png' alt='Yeongtaek Lee' />
                     <h3>Yeongtaek Lee</h3>
                     <p>Developer</p>
                     <a className={styles.linkedin} href='https://www.linkedin.com/in/yeongtaek-lee' target="_blank"><img src='/linkedin.svg' alt='Linkedin Link' /></a>
